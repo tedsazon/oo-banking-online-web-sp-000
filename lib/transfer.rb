190704165c994e5bf0263adb3d2979d5c7e5312f
Ted Sazon <tedsazon@gmail.com>
2019-01-07 21:23:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
-  attr_accessor :status
+  attr_accessor :status :amount
   attr_reader :sender, :receiver,
 
   def initialize(sender, receiver)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = 0
 
 end
