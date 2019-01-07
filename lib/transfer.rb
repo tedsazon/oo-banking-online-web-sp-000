0bf6185d8e8c99a634467fe140363e8c3e255140
Ted Sazon <tedsazon@gmail.com>
2019-01-07 21:33:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,5 +15,7 @@ class Transfer
 
   def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending"
-
+    sender.balance -= amount
+    receiver.balance += amount
+    self.status = "complete"
 end
